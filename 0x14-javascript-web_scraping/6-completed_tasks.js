14fd624cb3948aa1e8bc52170a6b8e42de250537
Eyosiyas <realgaz.eys@gmail.com>
2023-07-25 05:58:02 -0700

    task 6 update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,33 +2,21 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
-
-request(apiUrl, function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Error:', response.statusCode, response.statusMessage);
-  } else {
-    try {
-      const todos = JSON.parse(body);
-      const usersWithCompletedTasks = todos.reduce((acc, todo) => {
-        if (todo.completed) {
-          if (!acc[todo.userId]) {
-            acc[todo.userId] = 1;
-          } else {
-            acc[todo.userId]++;
-          }
-        }
-        return acc;
-      }, {});
-      
-      for (const userId in usersWithCompletedTasks) {
-        console.log(`User ${userId} has completed ${usersWithCompletedTasks[userId]} tasks.`);
-      }
-    } catch (e) {
-      console.error('Error parsing the response body:', e);
-    }
+    console.error(error);
   }
+  const dict = JSON.parse(body).reduce((acc, elem) => {
+    if (!acc[elem.userId]) {
+      if (elem.completed) {
+        acc[elem.userId] = 1;
+      }
+    } else {
+      if (elem.completed) {
+        acc[elem.userId] += 1;
+      }
+    }
+    return acc;
+  }, {});
+  console.log(dict);
 });
-
