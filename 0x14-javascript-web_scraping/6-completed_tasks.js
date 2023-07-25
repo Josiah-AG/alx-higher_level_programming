6074b89d32ac6b93b44ff988bb17df6aec9a2b35
Eyosiyas <realgaz.eys@gmail.com>
2023-07-25 05:48:03 -0700

    task 6 update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,18 +5,23 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
-  }
-  const dict = JSON.parse(body).reduce((acc, elem) => {
-    if (!acc[elem.userId]) {
-      if (elem.completed) {
-        acc[elem.userId] = 1;
-      }
-    } else {
-      if (elem.completed) {
-        acc[elem.userId] += 1;
-      }
+  } else {
+    try {
+      const dict = JSON.parse(body).reduce((acc, elem) => {
+        if (!acc[elem.userId]) {
+          if (elem.completed) {
+            acc[elem.userId] = 1;
+          }
+        } else {
+          if (elem.completed) {
+            acc[elem.userId] += 1;
+          }
+        }
+        return acc;
+      }, {});
+      console.log(dict);
+    } catch (e) {
+      console.error('Error parsing the response body:', e);
     }
-    return acc;
-  }, {});
-  console.log(dict);
+  }
 });
